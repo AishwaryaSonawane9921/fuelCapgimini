d60eecf44ddee8cd6857fe7faaf92e34896553c3
AishwaryaSonawane9921 <aishwaryasonawane2001@gmail.com>
2022-06-05 12:28:36 +0530

    Initial Commit

---
+++ DAY34_JPA_onetomany/ApplicationManagerusingJpa/src/test/java/com/example/ApplicationManagerusingJpa/ApplicationManagerusingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApplicationManagerusingJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationManagerusingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
