bb8e480ac2a9bc61b340005846140480fe33a336
AishwaryaSonawane9921 <aishwaryasonawane2001@gmail.com>
2022-06-05 12:30:49 +0530

    Initial Commit

---
+++ DAY46_MVC/demoCartShopping/demoCartShopping/src/main/java/com/example/demoCartShopping/DemoCartShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCartShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCartShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCartShoppingApplication.class, args);
+	}
+
+}
---
+++ DAY46_MVC/demoCartShopping/demoCartShopping/src/test/java/com/example/demoCartShopping/DemoCartShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCartShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCartShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
